da06cc2ce62c5d92f86eb7ed5e3933308a1120b9
alanine <free-tip@hotmail.com>
2023-05-22 21:30:04 -0300

    Add files via upload

--- examples/python-samples/asticaVision_sample.py
+++ examples/python-samples/asticaVision_sample.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import base64
+import os
 
 def asticaAPI(endpoint, payload, timeout):
     response = requests.post(endpoint, data=payload, timeout=timeout, verify=False)
@@ -14,7 +16,20 @@ asticaAPI_timeout = 35 # seconds  Using "gpt" or "gpt_detailed" will increase re
 asticaAPI_endpoint = 'https://www.astica.org:9141/vision/describe'
 asticaAPI_modelVersion = '1.0_full' # '1.0_full' or '2.0_full'
 
-asticaAPI_input = 'https://www.astica.org/inputs/analyze_3.jpg' #or base64 encoded string: data:image/png;base64,iVBORw0KG.....
+#Input Method 1: https URL of a jpg/png image (faster)
+asticaAPI_input = 'https://www.astica.org/inputs/analyze_3.jpg' 
+
+'''
+#Input Method 2: base64 encoded string of a local image (slower)
+path_to_local_file = 'image.jpg';
+with open(path_to_local_file, 'rb') as file:
+    image_data = file.read()
+image_extension = os.path.splitext(path_to_local_file)[1]
+#For now, let's make sure to prepend appropriately with: "data:image/extension_here;base64" 
+asticaAPI_input = f"data:image/{image_extension[1:]};base64,{base64.b64encode(image_data).decode('utf-8')}"
+'''
+
+
 asticaAPI_visionParams = 'gpt,description,objects,faces' # comma separated options; leave blank for all; note "gpt" and "gpt_detailed" are slow.
 '''
     '1.0_full' supported options:
