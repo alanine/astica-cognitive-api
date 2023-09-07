d548d69a1f07ccb6e65fb16952db706ff41f53c9
alanine <free-tip@hotmail.com>
2023-09-07 02:45:41 -0300

    Add files via upload

--- examples/python/asticaListen_sample.py
+++ examples/python/asticaListen_sample.py
@@ -1,6 +1,18 @@
 import requests
 import json
-
+import base64
+import os
+def get_audio_base64_encoding(audio_path: str) -> str:
+    """
+    Function to return the base64 string representation of an audio file
+    """
+    with open(audio_path, 'rb') as file:
+        audio_data = file.read()
+    audio_extension = os.path.splitext(audio_path)[1]
+    base64_encoded = base64.b64encode(audio_data).decode('utf-8')
+    #Make sure to append base64 header
+    return f"data:audio/{audio_extension[1:]};base64,{base64_encoded}"
+    
 def asticaAPI(endpoint, payload, timeout):
     response = requests.post(endpoint, data=json.dumps(payload), timeout=timeout, headers={ 'Content-Type': 'application/json', })
     if response.status_code == 200:
@@ -10,15 +22,18 @@ def asticaAPI(endpoint, payload, timeout):
 
 asticaAPI_key = 'YOUR API KEY' # visit https://astica.ai
 asticaAPI_timeout = 25 # seconds
-
 asticaAPI_endpoint = 'https://listen.astica.ai/transcribe'
 asticaAPI_modelVersion = '1.0_full'
 
 asticaAPI_doStream = 0 # Determines whether to display responses in real-time.
 asticaAPI_low_priority = 0 # Lower costs by receiving a URL to query for results. 
 
-asticaAPI_input = 'https://astica.ai/example/asticaListen_sample.wav'
+if 1 == 1:
+    asticaAPI_input = 'https://astica.ai/example/asticaListen_sample.wav' # use https audio input (faster)
+else:
+    asticaAPI_input = get_audio_base64_encoding('input.wav')  # use base64 audio input (slower)
 
+    
 # Define payload dictionary
 asticaAPI_payload = {
     'tkn': asticaAPI_key,
