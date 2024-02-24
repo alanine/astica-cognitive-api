9d6e478aa49dbccf5ce8b197ed3084a18bb1d091
alanine <free-tip@hotmail.com>
2024-02-24 02:23:11 -0400

    Add files via upload

--- examples/node/asticaDesign_sample.js
+++ examples/node/asticaDesign_sample.js
@@ -9,11 +9,11 @@ var asticaAPI_endpoint = 'https://design.astica.ai/generate_image';
 var asticaAPI_modelVersion = '2.0_full';
 var asticaAPI_prompt = 'close-up photography of older gentleman standing in the rain at night, in a street lit by lamps'; 
 var asticaAPI_prompt_negative = '';
-var asticaAPI_moderate = 1; //1 = Moderate Generation; 0 = Allow NSFW
-var asticaAPI_generate_quality = 'faster';//high, standard, fast, faster
+var asticaAPI_generate_quality = 'faster'; //high, standard, fast, faster
 var asticaAPI_generate_lossless = 0; //0 = Default JPG, 1 = lossless uncompressed PNG
 var asticaAPI_seed = 0; //0 will randomize the seed for every generation
-var asticaAPI_low_priority = 0;
+var asticaAPI_moderate = 1; 
+var asticaAPI_low_priority = 0; //0 = realtime, 1 = low_priority (lower cost)
 
 
 //Prepare payload
@@ -22,10 +22,10 @@ var asticaAPI_payload = {
     modelVersion: asticaAPI_modelVersion,
     prompt: asticaAPI_prompt,
     prompt_negative: asticaAPI_prompt_negative,
-    moderate: asticaAPI_moderate,
     generate_quality: asticaAPI_generate_quality,
     generate_lossless: asticaAPI_generate_lossless,
     seed: asticaAPI_seed,
+    moderate: asticaAPI_moderate,
     low_priority: asticaAPI_low_priority,
 };
 
@@ -40,7 +40,9 @@ var asticaAPI_payload = {
         console.log('Error:', result.error);
     } else if (result.status === 'success') {
         if (result.resultURI) {
-            console.log('Output URI:', result.resultURI, '\nQuery this URL to obtain the output of your results');
+            console.log('===============');
+            console.log('Low Priority URI:', result.resultURI, '\nQuery this URL to obtain the output of your results');
+            console.log('===============');
         } else {
             console.log('===============');
             console.log('Generated Image:', result.output);
