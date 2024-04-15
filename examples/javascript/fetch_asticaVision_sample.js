3e0980f99012cbabfd6d2e93d59cf75cdba7ac03
alanine <free-tip@hotmail.com>
2024-04-15 01:15:26 -0300

    Sample update for model version 2.5_full

--- examples/javascript/fetch_asticaVision_sample.js
+++ examples/javascript/fetch_asticaVision_sample.js
@@ -1,10 +1,32 @@
 var asticaAPI_endpoint = 'https://vision.astica.ai/describe';
 var asticaAPI_payload = {
-    tkn: 'YOUR API KEY',  //visit https://astica.ai
-    modelVersion: '2.1_full', //1.0_full, 2.0_full, or 2.1_full 
-    input: 'https://www.astica.org/inputs/analyze_3.jpg', //https url or base64 (data:image/png;base64,...)
-    visionParams: '' //comma separated options; leave blank for all. See https://astica.ai/vision/documentation/#parameters
+    tkn: 'API KEY HERE',  //visit https://astica.ai
+    modelVersion: '2.5_full', //1.0_full, 2.0_full, 2.1_full or 2.5_full
+    input: 'https://www.astica.org/inputs/analyze_3.jpg', //https url or base64 encoded string
+    visionParams: 'gpt, describe, describe_all, tags, faces', //comma separated, leave blank for all. See below for more
+    gpt_prompt: '', // only used if visionParams includes "gpt" or "gpt_detailed"
+    prompt_length: 95, // number of words in GPT response
+    objects_custom_kw: '' // only used if visionParams includes "objects_custom" (v2.5_full or higher)
 };
+
+/*        
+    '2.5_full' supported visionParams: https://astica.ai/vision/documentation/#parameters
+        describe
+        describe_all
+        gpt (or) gpt_detailed 
+        text_read
+        objects
+        objects_custom
+        objects_color
+        categories
+        moderate
+        tags
+        color
+        faces
+        celebrities
+        landmarks
+        brands        
+*/
 
 fetch(asticaAPI_endpoint, {
     method: 'post',
@@ -17,7 +39,11 @@ fetch(asticaAPI_endpoint, {
 .then(response => response.json()) // convert to json
 .then(data => { //print data to console
     console.log("astica Vision AI Results")
-    console.log(data)
+    console.log(JSON.stringify(data))
+    /*
+        handle individual data points:
+        console.log("Caption:", data.caption.text)
+    */
 }) 
 .catch(error => {  // catch any errors
     console.log('Error:', error) 
